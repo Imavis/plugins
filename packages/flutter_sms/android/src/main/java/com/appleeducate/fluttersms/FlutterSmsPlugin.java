929e19c0074ac9c1c78a850ccbaad1358c812654
kunda <colombini.luca010@tiscali.it>
2019-03-28 15:27:53 +0100

    repo cleaned from .idea

--- packages/flutter_sms/android/src/main/java/com/appleeducate/fluttersms/FlutterSmsPlugin.java
+++ packages/flutter_sms/android/src/main/java/com/appleeducate/fluttersms/FlutterSmsPlugin.java
@@ -1,51 +1,57 @@
 package com.appleeducate.fluttersms;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.provider.Settings;
-import android.util.Log;
 
 import java.util.ArrayList;
 
-import androidx.core.app.ActivityCompat;
+import androidx.annotation.NonNull;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** FlutterSmsPlugin */
+/**
+ * FlutterSmsPlugin
+ */
 public class FlutterSmsPlugin implements MethodCallHandler {
-  Activity activity;
+    private Activity activity;
 
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_sms");
-    channel.setMethodCallHandler(new FlutterSmsPlugin(registrar.activity()));
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("sendSMS")) {
-      String message = call.argument("message");
-      ArrayList<String> recipients = call.argument("recipients");
-      sendSMS(recipients, message);
-      result.success("SMS Sent!" );
-    } else {
-      result.notImplemented();
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_sms");
+        channel.setMethodCallHandler(new FlutterSmsPlugin(registrar.activity()));
     }
-  }
 
-  private FlutterSmsPlugin(Activity activity) {
-    this.activity = activity;
-  }
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("sendSMS")) {
+            String message = call.argument("message");
+            ArrayList<String> recipients = call.argument("recipients");
+            if (message != null && recipients != null) {
+                sendSMS(recipients, message);
+                result.success("SMS Sent!");
+            } else {
+                result.error("Error in sending sms, recipient or message is null", null, null);
+            }
+        } else {
+            result.notImplemented();
+        }
+    }
 
-  private void sendSMS(ArrayList<String> phones, String message) {
-    Intent sendIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phones));
-    sendIntent.putExtra("sms_body", message);
-    activity.startActivity(sendIntent);
-  }
+    private FlutterSmsPlugin(Activity activity) {
+        this.activity = activity;
+    }
+
+    private void sendSMS(@NonNull ArrayList<String> phones, @NonNull String message) {
+        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+        sendIntent.putExtra("address", phones.get(0));
+        sendIntent.putExtra("sms_body", message);
+        sendIntent.setData(Uri.parse("smsto:" + phones.get(0)));
+        activity.startActivity(sendIntent);
+    }
 }
